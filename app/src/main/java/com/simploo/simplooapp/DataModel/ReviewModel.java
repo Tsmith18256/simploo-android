719c6f0675423bd6a0d2f50ce83f5f33efea4773
Jerry Tsai <jerry604@gmail.com>
2016-09-23 18:49:50 -0700

    fix case sensitivity

---
+++ app/src/main/java/com/simploo/simplooapp/DataModel/ReviewModel.java
@@ -0,0 +1,14 @@
+package com.simploo.simplooapp.DataModel;
+
+/**
+ * Created by user on 16-09-23.
+ */
+public class ReviewModel {
+    //All ID's used.
+    private long review_id;
+    private long user_id;
+    private long washroom_id;
+
+    //Body of the review
+    private String description;
+}
