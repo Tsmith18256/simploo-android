11ea5e6461d3c70bb507703b98db482abe8d86ef
Jerry Tsai <jerry604@gmail.com>
2016-09-24 12:21:43 -0700

    initial map state

--- app/src/main/java/com/simploo/simplooapp/MainActivity.java
+++ app/src/main/java/com/simploo/simplooapp/MainActivity.java
@@ -3,11 +3,26 @@ package com.simploo.simplooapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private MapFragment mMapFragment;
+    private GoogleMap gMap;
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
     }
 }
