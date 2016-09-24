7d3ff8bb7e6de455ea7452913ae54158686ee78e
Jerry Tsai <jerry604@gmail.com>
2016-09-23 18:25:03 -0700

    data models

---
+++ app/src/main/java/com/simploo/simplooapp/DataModel/reviewModel.java
@@ -0,0 +1,7 @@
+package com.simploo.simplooapp.DataModel;
+
+/**
+ * Created by user on 16-09-23.
+ */
+public class reviewModel {
+}
---
+++ app/src/main/java/com/simploo/simplooapp/DataModel/washroomModel.java
@@ -0,0 +1,15 @@
+package com.simploo.simplooapp.DataModel;
+
+/**
+ * Created by Jerry Tsai on 16-09-23.
+ */
+public class washroomModel {
+
+    private long id;
+    private String name;
+    private String description;
+    private float lat;
+    private float lng;
+
+    private boolean hasWheelchairAccess;
+}
