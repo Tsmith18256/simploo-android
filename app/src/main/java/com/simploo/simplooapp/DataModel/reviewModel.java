221e4a5eb53d5e062c99bcd89e936705d35e1169
nuelz <macbook@MacBook-Pro-Retina.local>
2016-09-23 18:40:12 -0700

    - Renamed reviewModel to ReviewModel because it's a class

--- app/src/main/java/com/simploo/simplooapp/DataModel/reviewModel.java
+++ app/src/main/java/com/simploo/simplooapp/DataModel/reviewModel.java
@@ -3,5 +3,12 @@ package com.simploo.simplooapp.DataModel;
 /**
  * Created by user on 16-09-23.
  */
-public class reviewModel {
+public class ReviewModel {
+    //All ID's used.
+    private long review_id;
+    private long user_id;
+    private long washroom_id;
+
+    //Body of the review
+    private String description;
 }
